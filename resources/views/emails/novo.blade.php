0d44a6665550ce2a9006f186098c1fbb43bdea4d
Wali Queiroz <wsantos077@gmail.com>
2019-11-21 10:58:52 -0300

    Correção no email

--- resources/views/emails/novo.blade.php
+++ resources/views/emails/novo.blade.php
@@ -2,7 +2,7 @@
 
 <body>
     <p>Olá, {{ $usuario->nome }}!</p>
-    <p>Seja bem vindo ao sistema! Utilize seu link de indicação para converter novos usuários.</p>
+    <p>Seja bem vindo ao sistema teste FFWD! Utilize seu link de indicação para converter novos usuários.</p>
     <p>Att, <br>
         Administrador </p>
 </body>
